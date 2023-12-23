c1dbeada5621156ead31e593c6c2ecd0784f2a15
ivenskikh <ivenskikh@gmail.com>
2023-12-23 14:29:39 +0500

    lesson1: task4

---
+++ task4/Program.cs
@@ -0,0 +1,27 @@
+﻿int a = 6510;
+int b = 1654;
+int c = 87;
+int d = 9;
+int e = 65;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+
+Console.WriteLine(max);
+    
